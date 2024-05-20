c38997227ee9dd921cd67f928bbd880d812636b9
sir-noorhasan85 <161638862+noorahmed85@users.noreply.github.com>
2024-05-21 00:41:59 +0500

    Add files via upload

---
+++ asymnt.ts
@@ -0,0 +1,61 @@
+// // // QUESTION #1
+// let fruits = ["apple", "banana", "orange", "mango"];
+// console.log(fruits);
+
+
+// // // QUESTION #2
+// let numbers = [10, 20, 30, 40, 50];
+// console.log(numbers);
+
+
+// // QUESTION #3
+// let fruits = ["apple", "banana", "orange", "mango"];
+// let lastFruit = fruits.pop();
+// console.log(lastFruit);
+
+
+// // // QUESTION #4
+// let numbers = [10, 20, 30, 40, 50];
+// numbers [1] = 25; // now 2nd elemmt of th num is 25
+// console.log(numbers);
+
+
+// // // // QUESTION #5
+// let fruits = ["apple", "banana", "orange", "mango"];
+// fruits.push(`grape`);
+// console.log(fruits);
+
+// // // QUESTION #6
+// let fruits = ["apple", "banana", "orange", "mango"];
+// let lastFruit =  fruits.pop();
+// console.log( fruits ,"Your lastFruit Is ", lastFruit);
+
+// // // QUESTION #7
+// let fruits = ["apple", "banana", "orange", "mango"];
+// let firstFruit =  fruits.shift();
+// console.log("fruits is", fruits ,"& Your firstFruit Is ", firstFruit);
+
+// // // QUESTION #8
+// let fruits = ["apple", "banana", "orange", "mango"];
+// fruits.unshift("kiwi");
+// console.log(fruits);
+
+// // QUESTION #9
+// let fruits = ["apple", "banana", "orange", "mango"];
+// fruits.splice(0, 2);
+// console.log(fruits);
+
+// // // QUESTION #10
+// let fruits = ["apple", "banana", "orange", "mango"];
+// let citrusFruits = [0, 1]
+// console.log(fruits);
+
+// // // question 11;
+// let fruits = ["apple","banana","cherry","mango"];
+// let cirtusFruits = fruits.slice(0, 1);
+// console.log(cirtusFruits);
+
+// // // question 12;
+// let fruits = ["apple","banana","cherry","mango"];
+// let lastTwoFruits = fruits.slice(-2);
+// console.log(lastTwoFruits);
